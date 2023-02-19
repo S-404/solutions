708297f32bb47f0cab919fd42f8198eef8bb2c8a
S-404 <sudarev.andrey.i@gmail.com>
2023-02-19 08:56:43 +0300

    02 success

--- leetcode/02_AddTwoNumbers.js
+++ leetcode/02_AddTwoNumbers.js
@@ -37,30 +37,99 @@ var addTwoNumbers = function (l1, l2) {
             arr.length > 1 ? arrToList(arr.slice(1)) : null
         ) :
         null
-    const numFromList = list => Number(listToArr(list).reverse().join(''))
-    const strToNum = str => Number(str)
-    const sumArr = Array.from(String(numFromList(l1) + numFromList(l2)), strToNum).reverse() 
-    return arrToList(sumArr)
+
+    const sumArr = (arr1, arr2) => {
+        let resultArr = []
+        let mem = 0
+        for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
+            let getValue = arr => arr.length > i ? arr[i] : 0
+            let sum = getValue(arr1) + getValue(arr2) + mem
+            if (sum >= 10) {
+                resultArr.push(sum % 10)
+                mem = 1
+            } else {
+                resultArr.push(sum)
+                mem = 0
+            }
+        }
+        if (mem) resultArr.push(mem)
+        return resultArr.concat()
+    }
+
+    const sumArrResult = sumArr(listToArr(l1), listToArr(l2))
+    return arrToList(sumArrResult)
 
 };
 
+const testArrs = (arr1, arr2) => {
+    const sumArr = (arr1, arr2) => {
+        let resultArr = []
+        let mem = 0
+        for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
+            let getValueFromArr = arr => arr.length > i ? arr[i] : 0
+            let sum = getValueFromArr(arr1) + getValueFromArr(arr2) + mem
+            if (sum >= 10) {
+                resultArr.push(sum % 10)
+                mem = 1
+            } else {
+                resultArr.push(sum)
+                mem = 0
+            }
+        }
+        if (mem) resultArr.push(mem)
+        return resultArr.concat()
+    }
+
+    const sumArrResult = sumArr(arr1, arr2)
+    return sumArrResult
+}
+
+// const cases = [
+//     {
+//         l1: { "val": 2, "next": { "val": 4, "next": { "val": 3, "next": null } } },
+//         l2: { "val": 5, "next": { "val": 6, "next": { "val": 4, "next": null } } },
+//     },
+//     {
+//         l1: { "val": 0, "next": null },
+//         l2: { "val": 0, "next": null },
+//     },
+//     {
+//         l1: { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": null } } } } } } },
+//         l2: { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": null } } } },
+//     }
+// ]
+
+// let count = 0
+// for (let c of cases) {
+//     console.log(`case:"${count}" converted to "${JSON.stringify(addTwoNumbers(c.l1, c.l2))}"`)
+//     count++
+// }
+
+
+
 const cases = [
     {
-        l1: { "val": 2, "next": { "val": 4, "next": { "val": 3, "next": null } } },
-        l2: { "val": 5, "next": { "val": 6, "next": { "val": 4, "next": null } } },
+        l1: [2, 4, 3],
+        l2: [5, 6, 4],
     },
     {
-        l1: { "val": 0, "next": null },
-        l2: { "val": 0, "next": null },
+        l1: [0],
+        l2: [0],
     },
     {
-        l1: { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": null } } } } } } },
-        l2: { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": { "val": 9, "next": null } } } },
+        l1: [9, 9, 9, 9, 9, 9, 9],
+        l2: [9, 9, 9, 9],
     }
 ]
 
 let count = 0
 for (let c of cases) {
-    console.log(`case:"${count}" converted to "${JSON.stringify(addTwoNumbers(c.l1, c.l2))}"`)
+    console.log(`case:"${count}" converted to "${JSON.stringify(testArrs(c.l1, c.l2))}"`)
     count++
 }
+
+
+//999
+//599
+
+// 98
