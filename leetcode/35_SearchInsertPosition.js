94e45e256cda8b0cb6b8756eaafb859945950f35
S-404 <sudarev.andrey.i@gmail.com>
2023-02-27 15:44:39 +0300

    leetcode 35 solved

--- leetcode/35_SearchInsertPosition.js
+++ leetcode/35_SearchInsertPosition.js
@@ -39,16 +39,48 @@ nums contains distinct values sorted in ascending order.
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
-    
+var searchInsert = function (nums, target) {
+
+    if (target > nums[nums.length - 1]) return nums.length
+    if (target < nums[0] || nums.length <= 1) return 0
+
+    const getNewIndex = (start, end) => Math.ceil((end - start) / 2)
+
+    let searchStartIndex = 0
+    let searchEndIndex = nums.length
+    let searchIndex = getNewIndex(searchStartIndex, searchEndIndex)
+
+    while (searchIndex >= 0 && searchIndex < nums.length) {
+
+        if (target === nums[searchIndex]) return searchIndex
+
+        if (searchEndIndex === searchStartIndex) return searchStartIndex
+        
+        if (target > nums[searchIndex]) {
+            searchStartIndex = searchIndex + 1
+            searchIndex += getNewIndex(searchStartIndex, searchEndIndex)
+            continue
+        }
+        if (target < nums[searchIndex]) {
+            searchEndIndex = searchIndex
+            searchIndex -= getNewIndex(searchStartIndex, searchEndIndex)
+            continue
+        }
+
+    }
+    return searchIndex + 1
 };
 
-
+let start1 = Date.now()
 let cases = [
-    { nums: [1,3,5,6], val: 5 },
-    { nums: [1,3,5,6], val: 2 },
-    { nums: [1,3,5,6], val: 7 },
+    { nums: [1, 3, 5, 6], val: 5 },
+    { nums: [1, 3, 5, 6], val: 4 },
+    { nums: [1, 3, 5, 6], val: 7 },
+    { nums: [1, 3], val: 2 },
+    { nums: [1], val: 1 },
 ]
 for (let c of cases) {
     console.log(`case:"${c.val}" - result:"${searchInsert(c.nums, c.val)}"`)
 }
+let end1 = Date.now()
+console.log('completed', end1 - start1)
