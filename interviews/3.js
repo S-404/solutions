fff216eac8605d644c0ae1c3af55c24c149fc8b3
S-404 <sudarev.andrey.i@gmail.com>
2023-03-01 13:24:41 +0300

    solved interview 03

--- interviews/3.js
+++ interviews/3.js
@@ -14,6 +14,19 @@ output: undefined
 */
 
 const get = (keySequence, nestedObject) => {
+    let keys = keySequence.split('.')
+    let depth = -1
+    const parseLinks = (nestedObject) => {
+        depth++
+        if(depth >= keys.length) return
+        if (nestedObject[keys[depth]]) {
+            return typeof nestedObject[keys[depth]] === 'object' ?
+                parseLinks(nestedObject[keys[depth]]) :
+                nestedObject[keys[depth]]
+        }
+    }
+    let result = parseLinks(nestedObject)
+    return result
 
 }
 
@@ -30,5 +43,6 @@ const cases = [
 
 
 for (let c of cases) {
-    console.log(`case: ${c.keySequence} return ${get(c.keySequence, c.nestedObject)}`)
+    let result = get(c.keySequence, c.nestedObject)
+    console.log(`case: ${c.keySequence} return ${result}`)
 }
