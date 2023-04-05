cb33e575d008389783552aa7ecbd333e6409e8e9
andrey_s <sudarev.andrey.i@gmail.com>
2023-04-05 08:33:22 +0300

    init leetcode 0022

---
+++ leetcode/0022_GenerateParentheses.js
@@ -0,0 +1,27 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+ 
+Constraints:
+1 <= n <= 8
+*/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    
+};
+
+let cases = [3, 1, 4, 2]
+for (let c of cases) {
+    console.log(`case:"${c}" - result:"${generateParenthesis(c)}"`)
+}
